ff4778e024c5dcf866e4ad86e044953a086a6638
Kalindhu Navanjana <kalindhunavanjana@gmail.com>
2023-08-09 04:27:07 +0530

    expiration of auth tokens

--- routes/auth.js
+++ routes/auth.js
@@ -8,27 +8,21 @@ const router = express.Router();
 
 router.post("/login", (req, res) => {
     //Authenticate user
-    // const user = {
-    //     id: 1,
-    //     username: "John Doe",
-    //     email: req.body.email,
-    // };
-    // res.json(user);
-    console.log("first")
-    
+
     const username = req.body.username;
     // const password = req.body.password;
-    console.log(req.body)
-    
+    console.log(req.body);
+
     const user = {
         id: 1,
         username: username,
         //other fields go here
-    }
+    };
 
-    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
-    res.json({accessToken: accessToken});
-
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    });
+    res.json({ accessToken: accessToken });
 });
 
 module.exports = router;
